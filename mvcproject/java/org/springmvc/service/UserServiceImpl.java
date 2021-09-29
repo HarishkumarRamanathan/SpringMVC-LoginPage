8bd696a2fb6d5cc427efcfa8ad1ba750c68eeb03
User <harishkumar.rcse@ksrce.ac.in>
2021-09-29 11:42:46 +0530

    Login page

---
+++ mvcproject/java/org/springmvc/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package org.springmvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springmvc.dao.UserDao;
+import org.springmvc.model.Login;
+import org.springmvc.model.User;
+
+public class UserServiceImpl implements UserService {
+
+	  @Autowired
+	  public UserDao userDao;
+
+	  public int register(User user) {
+	    return userDao.register(user);
+	  }
+
+	  public User validateUser(Login login) {
+	    return userDao.validateUser(login);
+	  }
+
+	}	
